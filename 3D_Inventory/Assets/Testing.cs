d025edff0ce490e0f2ba21a0aed252eca7cb5c8f
hjbenitez <humbertobenitez5760@yahoo.ca>
2022-10-06 19:31:52 -0400

    Fixed bug, now the proper cell updates when clicked on

--- 3D_Inventory/Assets/Testing.cs
+++ 3D_Inventory/Assets/Testing.cs
@@ -14,15 +14,23 @@ public class Testing : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            //grid.setValue(GetMouseWorldPosition(), 56);
             grid.setValue(GetMouseWorldPosition(), 56);
+
         }
     }
 
 
+    public Vector3 GetMouseWorldPosition()
+    {
+        Vector3 screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.gameObject.transform.position.z);
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPoint);
+        Debug.Log(worldPosition);
+        return worldPosition;
+        
+    }
 
-
-
-
+    /*
     public Vector3 GetMouseWorldPosition()
     {
         Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
@@ -42,4 +50,5 @@ public class Testing : MonoBehaviour
         Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
         return worldPosition;
     }
+    */
 }
