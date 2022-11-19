412ec620ae810c54edac8f1f970543a97e0a0f3b
Humberto Benitez <humbertobenitez5760@yahoo.ca>
2022-11-19 17:08:05 -0500

    rotation along the x-axis now functioning

---
+++ 3D_Inventory/Assets/CameraRotate.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    Vector3 newPosition;
+    bool rotating;
+
+    Vector3 backpack = new Vector3(30, 30, 0);
+
+    float radius = -45.1f;
+
+    public float rotationSpeed = 25f;
+
+    float elevationOffset = 0;
+
+    Vector3 positionOffset;
+    public float angle = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            rotating = true;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            rotating = false;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (rotating)
+        {
+            float horizontalDirection = Input.GetAxis("Mouse X");
+            //float verticalDirection = Input.GetAxis("Mouse Y");
+
+            positionOffset.Set(
+                Mathf.Cos(angle) * radius,
+                elevationOffset,
+                Mathf.Sin(angle) * radius    
+            );
+
+
+            transform.position = backpack + positionOffset;
+            angle += Time.deltaTime * rotationSpeed * horizontalDirection;
+            transform.LookAt(backpack);
+            
+        }
+    }
+}
